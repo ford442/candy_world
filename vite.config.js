7fdeef7bed92dab571755d19528084cebc5638b0
Noah Cohn <noahc42@gmail.com>
2025-12-03 09:48:52 -0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
-  }
-  ,
+  },
+  plugins: [
+    basicSsl(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/libopenmpt/lib/libopenmpt.js',
+          dest: 'assets'
+        },
+        {
+          src: 'node_modules/libopenmpt/lib/libopenmpt.wasm',
+          dest: 'assets'
+        }
+      ]
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
