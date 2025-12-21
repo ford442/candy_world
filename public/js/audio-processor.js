d2054d003552b702a1760d4f307ecd60bb3b18eb
Noah Cohn <noahc42@gmail.com>
2025-12-21 01:11:56 +0000

    push fix

--- public/js/audio-processor.js
+++ public/js/audio-processor.js
@@ -2,7 +2,7 @@
 
 // 1. Use ES Module Import instead of importScripts
 // Note: This expects libopenmpt.js to be accessible at /js/libopenmpt.js
-import './libopenmpt.js'; 
+import 'https://test.1ink.us/candy-world/js/libopenmpt.js'; 
 
 // Helper functions 
 const lerp = (a, b, t) => a + (b - a) * t;
