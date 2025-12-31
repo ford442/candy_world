090f58c23ec0da78e3787c590fcabbeb12cb8168
Noah Cohn <noahc42@gmail.com>
2025-12-31 12:25:58 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
   server: {
     headers: {
       // These headers are REQUIRED for SharedArrayBuffer (Pthreads)
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+      // 'Cross-Origin-Opener-Policy': 'same-origin',
+     //  'Cross-Origin-Embedder-Policy': 'require-corp',
     }
   },
   // Ensure the worker file is treated correctly if using Vite's worker import (optional but safe)
