772a035cc844e827ec160cb7774c953101a6fd71
Noah Cohn <noahc42@gmail.com>
2026-01-02 08:35:35 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   server: {
     headers: {
       // These headers are REQUIRED for SharedArrayBuffer (Pthreads)
